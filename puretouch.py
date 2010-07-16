0f97080dc2a1236d2e890bc5c4054a1e38f1a5ad
Alan Fachini <alfakini@gmail.com>
2010-07-15 22:59:33 -0300

    now, moving objects

--- puretouch.py
+++ puretouch.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 from pymt import *
 from pdpatch import *
+from pdobjects import *
 import os
 
 if __name__ == "__main__":
     #os.system('pd topd/recebe.pd & sleep 2000')
     window = MTWindow()
     patch = PDPatch(size=window.size)
+    n1 = PDNumber(pdpatch=patch.pdpatch, pos=(100,100))
+    n2 = PDNumber(pdpatch=patch.pdpatch, pos=(10,10))
+    patch.add_widget(n1)
+    patch.add_widget(n2)
     window.add_widget(patch)
     runTouchApp()
